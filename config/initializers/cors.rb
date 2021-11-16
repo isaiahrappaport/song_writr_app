92212bc904d20ffd92305e61f8593df2c9b16d9a
Isaiah Rappaport <irappap98@gmail.com>
2021-11-16 18:15:29 -0500

    config.assets.compile = true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://hopeful-austin-bff835.netlify.app"
+    origins "localhost:8080", "https://hopeful-austin-bff835.netlify.app"
 
     resource "*",
       headers: :any,
